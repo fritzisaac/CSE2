dc7c28bf902b8fa8047b211a01e861c228356602
Fritz Isaac <fji218@lehigh.edu>
2014-08-27 17:04:57 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+///////////////////////
+//Fritz Isaac
+//hw 01
+//Welcome Class Java Program
+//
+//   javac WelcomeClass.java
+//
+//   java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+    public static void main(String[ ]  args) {
+        
+// print welcome design
+    System.out.println("   ------------");
+    System.out.println("   | WELCOME  |");
+    System.out.println("   ------------");
+    System.out.println("   ^  ^  ^  ^  ^  ^");
+    System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println(" |-F--J--I--2--1--8-|");
+    System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("   v  v  v  v  v  v ");
+    System.out.println(" I run on the track and field team at Lehigh University. I hurdle and triple jump.");
+    
+    } 
+    
+    }
