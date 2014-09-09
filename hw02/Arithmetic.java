7dd9f3b62961e3de41603345b5ae87e070e7744f
Fritz Isaac <fji218@lehigh.edu>
2014-09-09 04:19:47 +0000

    updated Arithmetic

--- hw02/Arithmetic.java
+++ hw02/Arithmetic.java
@@ -33,16 +33,16 @@
     double taxPercent=0.06;
 
 //eequations
-    double taxSock$=taxPercent*sockCost$, //tax on socks
-    taxGlass$=taxPercent*glassCost$, //tax on glasses
-    taxEnvelope$=taxPercent*envelopeCost$, //tax on envelopes
+    double taxSock$=taxPercent*sockCost$*100, //tax on socks
+    taxGlass$=taxPercent*glassCost$*100, //tax on glasses
+    taxEnvelope$=taxPercent*envelopeCost$*100, //tax on envelopes
    
     noTaxCostSock$=nSocks*sockCost$, //total cost of socks without tax
     noTaxCostGlass$=nGlasses*glassCost$, //total cost of glasses without tax
     noTaxCostEnvelope$=nEnvelopes*envelopeCost$, //total cost of envelopes without tax
    
-    noTaxTotalCost$=noTaxCostEnvelope$+noTaxCostGlass$+noTaxCostSock$, //total cost of items without tax
-    taxTotalCost$=(taxPercent*noTaxTotalCost$)+noTaxTotalCost$; //total amount paid for items plus sales tax
+    noTaxTotalCost$=(noTaxCostEnvelope$+noTaxCostGlass$+noTaxCostSock$)*100, //total cost of items without tax
+    taxTotalCost$=((taxPercent*noTaxTotalCost$)+noTaxTotalCost$); //total amount paid for items plus sales tax
     
     int tS$ = (int)taxSock$;
     int tG$=(int)taxGlass$;
@@ -50,16 +50,24 @@
     int nTTC$=(int)noTaxTotalCost$;
     int tTC$=(int)taxTotalCost$;
     
+    double tS1$= (double)tS$/100;
+    double tG1$=(double)tG$/100;
+    double tE1$=(double)tE$/100;
+    double nTTC1$=(double)nTTC$/100;
+    double tTC1$=(double)tTC$/100;
+
+    
+    
 //print no tax item costs
 System.out.println("I purchased "+nSocks+" socks at a price of "+sockCost$+" dollars each. "); //# of socks purchased & ind. price
 System.out.println("I purchased "+nGlasses+" glasses at a price of "+glassCost$+" dollars each. "); //# of glasses purchased & ind. price
 System.out.println("I purchased "+nEnvelopes+" envelope at a price of "+envelopeCost$+" dollars. "); //# of envelopes purchased & ind. price
 
-System.out.println("The socks cost "+noTaxCostSock$+" dollars plus "+tS$+" in tax dollars."); //total cost of socks and tax for this cost
-System.out.println("The glasses cost "+noTaxCostGlass$+" dollars plus "+tG$+" in tax dollars."); //total cost of glasses and tax for this cost
-System.out.println("The envelope cost "+noTaxCostEnvelope$+" dollars plus "+tE$+" in tax dollars."); //total cost of envelopes and tax for this cost
-System.out.println("The purchase cost "+nTTC$+" dollars total without tax."); //total purchase cost without tax
-System.out.println("The total cost of my purchase with tax was "+tTC$+" dollars." ); //total purchase cost with tax
+System.out.println("The socks cost "+noTaxCostSock$+" dollars plus "+tS1$+" in tax dollars."); //total cost of socks and tax for this cost
+System.out.println("The glasses cost "+noTaxCostGlass$+" dollars plus "+tG1$+" in tax dollars."); //total cost of glasses and tax for this cost
+System.out.println("The envelope cost "+noTaxCostEnvelope$+" dollars plus "+tE1$+" in tax dollars."); //total cost of envelopes and tax for this cost
+System.out.println("The purchase cost "+nTTC1$+" dollars total without tax."); //total purchase cost without tax
+System.out.println("The total cost of my purchase with tax was "+tTC1$+" dollars." ); //total purchase cost with tax
         
     }
     }
